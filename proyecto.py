8cadd528d44d22616a2477c1bb713caf1e3d9952
ncastrovilla <nicolascastrovilla@gmail.com>
2020-03-27 20:42:54 -0300

    Insert 90%

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request , redirect
 from flaskext.mysql import MySQL
 import pymysql
 import pymysql.cursors
@@ -14,16 +14,34 @@ mysql = MySQL(app)
 mysql.connect_args["autocommit"] = True 
 mysql.connect_args["cursorclass"] = pymysql.cursors.DictCursor
 
-@app.route('/pacientes')
+@app.route('/') 
 def pacientes():
 	cursor = mysql.get_db().cursor()
+
 	cursor.execute("SELECT * FROM Paciente")
 
 	paciente=cursor.fetchall()
 	return render_template('pacientes.html',pacient = paciente)
 
-@app.route('/pacientes/add')
+@app.route('/pacientes/add', methods=["GET","POST"])
 def padd():
+	cursor = mysql.get_db().cursor()
+
+	if request.method == "GET":
+		nombre = request.args.get('nombre', default = "", type = str)
+
+	if request.method == "POST":
+		nombre = request.form["nombre"]
+		run = request.form["run"]
+		nac = request.form["nacimiento"] 
+		try:	
+			sql = "INSERT INTO Paciente (id,nombre_paciente,rut,fecha_nacimiento)"
+			sql += " VALUES(default,%s,%s,%s)"
+			cursor.execute(sql,(nombre,run,nac))
+		except Exception as e:
+			print(e)
+		return redirect('/')
+
 	return render_template('nuevopaciente.html')
 
 @app.route('/vacunas')
@@ -34,10 +52,66 @@ def vacunas():
 	vacunas=cursor.fetchall()
 	return render_template('vacunas.html',vac = vacunas)
 
-@app.route('/vacunas/add')
+@app.route('/vacunas/add',  methods=["GET","POST"])
 def vadd():
+	cursor = mysql.get_db().cursor()
+
+	if request.method == "GET":
+		enfermedad = request.args.get('vacuna', default = "", type = str)
+
+	if request.method == "POST":
+		enfermedad = request.form["vacuna"]
+		try:	
+			sql = "INSERT INTO Vacuna (id,nombre_enfermedad)"
+			sql += " VALUES(default,%s)"
+			cursor.execute(sql,(enfermedad))
+		except Exception as e:
+			print(e)
+		return redirect('/vacunas')
 	return render_template('nuevavacuna.html')
 
+@app.route('/pacientes/vacunar', methods=["GET","POST"])
+def pvac():
+	cursor = mysql.get_db().cursor()
+
+	if request.method == "GET":
+		idp = request.args.get('id', default = "", type = str)
+
+	select="SELECT * FROM Paciente"
+	select+= " WHERE id = %s"
+
+	cursor.execute(select,(idp))
+
+	pacivac = cursor.fetchall()
+
+	cursor.execute("SELECT * FROM Vacuna")
+
+	vac = cursor.fetchall()
+
+	return render_template('vacunarpaciente.html', vacp= pacivac, vaco=vac)
+
+@app.route('/vacunas/pacientes')
+def vacp():
+	cursor = mysql.get_db().cursor()
+
+	if request.method == "GET":
+		idv = request.args.get('id', default = "", type = str)
+
+	select = "SELECT v.nombre_enfermedad,p.nombre_paciente,p.rut,r.fechavacuna FROM Vacuna as v, Paciente as p, recibe as r "
+	select += " WHERE r.idvacuna=v.id AND p.id=r.idpaciente AND v.id=%s"
+
+	cursor.execute(select,(idv))
+
+	vacupaci = cursor.fetchall()
+
+	select2 = "SELECT nombre_enfermedad FROM Vacuna"
+	select2 += " WHERE id=%s"
+
+	cursor.execute(select2,(idv))
+
+	nombre = cursor.fetchall()
+
+	return render_template('vacuna.html', vacuna=vacupaci, n=nombre)
 
 
 if __name__ == "__main__":
